7af8a0892db5c3d7e290c29f0d769c1cbf669b81
Johnson Mpeirwe <mpeirwe@gmail.com>
2013-03-18 12:10:16 +0300

    include amount in sync request

--- igreport_project/igreport_src/igreport/views/sync.py
+++ igreport_project/igreport_src/igreport/views/sync.py
@@ -55,6 +55,9 @@ def sync_report(request, report_id):
             'reference_number': report.reference_number,
             'complaint_date': datetime.strftime(report.datetime, '%Y/%m/%d'),
         }
+	if report.amount > 0:
+		report_data['amount'] = report.amount
+
         report_data = simplejson.dumps(report_data)
 
         cms_url = settings.CMS_URL
